4cfbf35fcf3d45e57a8668b662c116d1f656a09d
mends albert <mendsalbert@gmail.com>
2021-06-06 11:04:43 +0100

    added new changes

--- FRONTEND/MaterialUI-CSS/materialui-css/src/components/Header.js
+++ FRONTEND/MaterialUI-CSS/materialui-css/src/components/Header.js
@@ -12,7 +12,19 @@ export default function Header() {
     <>
       <body className="container">
         <div className="sidebar">SideBar</div>
-        <header className="header">Header</header>
+        <header className="header">
+          <img src={Logo} className="header__logo" />
+          <h3 className="heading-3">Your won home</h3>
+          <h1 className="heading-1">The ultimate personal freedom</h1>
+          <button className="btn">View our properties</button>
+          <div className="header__seenon-text">As Seen on</div>
+          <div className="header__seenon-logos">
+            <img src={Logo} />
+            <img src={Logo} />
+            <img src={Logo} />
+            <img src={Logo} />
+          </div>
+        </header>
         <div className="realtors">Top 3 realtors</div>
         <section className="features">
           <div className="feature">
@@ -93,11 +105,242 @@ export default function Header() {
             </p>
           </div>
         </section>
-        <div className="story__pictures">Story Pictures</div>
-        <div className="story__content">Story Content</div>
-        <section className="homes">Homes</section>
-        <section className="gallery">Gallery</section>
-        <footer className="footer">Footer</footer>
+        <div className="story__pictures">
+          <img src={hotel1} alt="img" className="story__img_1" />
+          <img src={hotel3} alt="img" className="story__img_2" />
+        </div>
+        <div className="story__content">
+          <h3 className="heading-3">Happy customers</h3>
+          <h2 className="heading-2 heading-2--dark">
+            &ldquo;The best decision of our lives&rdquo;
+          </h2>
+          <p className="story__text">
+            Lorem iopsn te fsleet sngn sensurus irupus Lorem iopsn te fsleet
+            sngn sensurus irupus sngn sensurus irupus Lorem iopsn te fsleet sngn
+            sensurus irupus Lorem iopsn te fsleet sngn sensurus irupus sngn
+            sensurus irupus
+          </p>
+          <button className="btn">Find your own home</button>
+        </div>
+        <section className="homes">
+          <div className="home">
+            <img src={hotel1} className="home_img" />
+            <svg className="home__icon">
+              <use href={sprite + "#icon-star"}></use>
+            </svg>
+            <h5 className="home__name">Beautiful Family House</h5>
+            <div className="home__location">
+              <svg>
+                <use href={sprite + "#icon-key"}></use>
+              </svg>
+              <p>USA</p>
+            </div>
+            <div className="home__rooms">
+              <svg>
+                <use href={sprite + "#icon-key"}></use>
+              </svg>
+              <p>USA</p>
+            </div>
+            <div className="home__area">
+              <svg>
+                <use href={sprite + "#icon-key"}></use>
+              </svg>
+              <p>USA</p>
+            </div>
+            <div className="home__price">
+              <svg>
+                <use href={sprite + "#icon-key"}></use>
+              </svg>
+              <p>USA</p>
+            </div>
+            <button className="btn home__btn">Contact realtors</button>
+          </div>
+
+          <div className="home">
+            <img src={hotel1} className="home_img" />
+            <svg className="home__icon">
+              <use href={sprite + "#icon-star"}></use>
+            </svg>
+            <h5 className="home__name">Beautiful Family House</h5>
+            <div className="home__location">
+              <svg>
+                <use href={sprite + "#icon-key"}></use>
+              </svg>
+              <p>USA</p>
+            </div>
+            <div className="home__rooms">
+              <svg>
+                <use href={sprite + "#icon-key"}></use>
+              </svg>
+              <p>USA</p>
+            </div>
+            <div className="home__area">
+              <svg>
+                <use href={sprite + "#icon-key"}></use>
+              </svg>
+              <p>USA</p>
+            </div>
+            <div className="home__price">
+              <svg>
+                <use href={sprite + "#icon-key"}></use>
+              </svg>
+              <p>USA</p>
+            </div>
+            <button className="btn home__btn">Contact realtors</button>
+          </div>
+
+          <div className="home">
+            <img src={hotel1} className="home_img" />
+            <svg className="home__icon">
+              <use href={sprite + "#icon-star"}></use>
+            </svg>
+            <h5 className="home__name">Beautiful Family House</h5>
+            <div className="home__location">
+              <svg>
+                <use href={sprite + "#icon-key"}></use>
+              </svg>
+              <p>USA</p>
+            </div>
+            <div className="home__rooms">
+              <svg>
+                <use href={sprite + "#icon-key"}></use>
+              </svg>
+              <p>USA</p>
+            </div>
+            <div className="home__area">
+              <svg>
+                <use href={sprite + "#icon-key"}></use>
+              </svg>
+              <p>USA</p>
+            </div>
+            <div className="home__price">
+              <svg>
+                <use href={sprite + "#icon-key"}></use>
+              </svg>
+              <p>USA</p>
+            </div>
+            <button className="btn home__btn">Contact realtors</button>
+          </div>
+
+          <div className="home">
+            <img src={hotel1} className="home_img" />
+            <svg className="home__icon">
+              <use href={sprite + "#icon-star"}></use>
+            </svg>
+            <h5 className="home__name">Beautiful Family House</h5>
+            <div className="home__location">
+              <svg>
+                <use href={sprite + "#icon-key"}></use>
+              </svg>
+              <p>USA</p>
+            </div>
+            <div className="home__rooms">
+              <svg>
+                <use href={sprite + "#icon-key"}></use>
+              </svg>
+              <p>USA</p>
+            </div>
+            <div className="home__area">
+              <svg>
+                <use href={sprite + "#icon-key"}></use>
+              </svg>
+              <p>USA</p>
+            </div>
+            <div className="home__price">
+              <svg>
+                <use href={sprite + "#icon-key"}></use>
+              </svg>
+              <p>USA</p>
+            </div>
+            <button className="btn home__btn">Contact realtors</button>
+          </div>
+
+          <div className="home">
+            <img src={hotel1} className="home_img" />
+            <svg className="home__icon">
+              <use href={sprite + "#icon-star"}></use>
+            </svg>
+            <h5 className="home__name">Beautiful Family House</h5>
+            <div className="home__location">
+              <svg>
+                <use href={sprite + "#icon-key"}></use>
+              </svg>
+              <p>USA</p>
+            </div>
+            <div className="home__rooms">
+              <svg>
+                <use href={sprite + "#icon-key"}></use>
+              </svg>
+              <p>USA</p>
+            </div>
+            <div className="home__area">
+              <svg>
+                <use href={sprite + "#icon-key"}></use>
+              </svg>
+              <p>USA</p>
+            </div>
+            <div className="home__price">
+              <svg>
+                <use href={sprite + "#icon-key"}></use>
+              </svg>
+              <p>USA</p>
+            </div>
+            <button className="btn home__btn">Contact realtors</button>
+          </div>
+
+          <div className="home">
+            <img src={hotel1} className="home_img" />
+            <svg className="home__icon">
+              <use href={sprite + "#icon-star"}></use>
+            </svg>
+            <h5 className="home__name">Beautiful Family House</h5>
+            <div className="home__location">
+              <svg>
+                <use href={sprite + "#icon-key"}></use>
+              </svg>
+              <p>USA</p>
+            </div>
+            <div className="home__rooms">
+              <svg>
+                <use href={sprite + "#icon-key"}></use>
+              </svg>
+              <p>5 rooms</p>
+            </div>
+            <div className="home__area">
+              <svg>
+                <use href={sprite + "#icon-key"}></use>
+              </svg>
+              <p>
+                325m<sup>2</sup>
+              </p>
+            </div>
+            <div className="home__price">
+              <svg>
+                <use href={sprite + "#icon-key"}></use>
+              </svg>
+              <p>$1,200,000</p>
+            </div>
+            <button className="btn home__btn">Contact realtors</button>
+          </div>
+        </section>
+        <section className="gallery">
+          <img src={hotel3} className="gallery_img gallery_img--1" />
+          <img src={hotel3} className="gallery_img gallery_img--2" />
+        </section>
+        <footer className="footer">
+          <div className="footer__text">
+            <button className="footer__btn">Find your dream home</button>
+            <button className="footer__btn">Request Proposal</button>
+            <button className="footer__btn">Download home planner</button>
+            <button className="footer__btn">Contact us</button>
+            <button className="footer__btn">Submit your property</button>
+            <button className="footer__btn">Come work with us!</button>
+          </div>
+          <p className="footer__copywrite">
+            Lorem iopsn te fsleet sngn sensurus irupus Lorem iopsn te fsleet
+            sngn sensurus irupus sngn sensurus irupus Lorem iopsn te fsleet sngn
+          </p>
+        </footer>
       </body>
     </>
   );
